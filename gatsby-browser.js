e1a092a3cf9200149789014e5257f6d4423c38e1
Sebastian Machaj <team@blackredstudio.com>
2021-02-08 22:44:42 +0100

    fix shouldUpdateScroll gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
 exports.shouldUpdateScroll = ({ routerProps, getSavedScrollPosition }) => {
-  if (routerProps.location.state.lang) return false;
+  if (routerProps?.location?.state?.lang) return false;
   return getSavedScrollPosition(routerProps.location);
 };
